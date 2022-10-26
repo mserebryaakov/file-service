9f71d3ebe8b7a40d757eff84ff311cb8e79ecae5
mserebryaakov <maks.serebriakov2019@gmail.com>
2022-10-26 14:08:27 +0300

    RW timeout change

--- pkg/httpserver/httpserver.go
+++ pkg/httpserver/httpserver.go
@@ -17,8 +17,8 @@ func (s *Server) Run(port string, handler http.Handler) error {
 		Addr:           port,
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    5 * time.Minute,
+		WriteTimeout:   5 * time.Minute,
 	}
 
 	return s.httpServer.ListenAndServe()
